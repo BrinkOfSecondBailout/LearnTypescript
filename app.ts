51d842a1afe4307cf945a13d32ae4b4508428122
Danny Nguyen <nguyenduongpa@gmail.com>
2023-11-15 12:43:27 -0800

    d

--- app.ts
+++ app.ts
@@ -2,20 +2,27 @@
 //     name: string;
 //     age: number
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string]
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string]
+// } = {
+//     name: "Max",
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+const person = {
     name: "Max",
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: 'READ ONLY USER'
 };
 
-person.role.push('admin');
-person.role[1] = 10;
+// person.role.push('admin');
+// person.role[1] = 'admin';
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
