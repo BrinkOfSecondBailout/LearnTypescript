5e4b68eb31d0b97ad583403927cbc5ca236a4ddb
Danny Nguyen <nguyenduongpa@gmail.com>
2023-11-15 16:22:43 -0800

    d

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
-// const person: {
-//     name: string;
-//     age: number
-// } = {
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string]
-// } = {
-//     name: "Max",
-//     age: 30,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// };
-var ADMIN = 0;
-var READ_ONLY = 1;
-var AUTHOR = 2;
-var person = {
-    name: "Max",
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: ADMIN
-};
-if (person.role === ADMIN) {
-    console.log('is admin');
+function combine(input1, input2) {
+    var result = input1 + input2;
+    return result;
 }
-// person.role.push('admin');
-// person.role[1] = 'admin';
-// let favoriteActivities: string[];
-// favoriteActivities = ['Sports'];
-// console.log(person);
-// for (const hobby of person.hobbies) {
-//     console.log(hobby.toUpperCase())
-// }
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
